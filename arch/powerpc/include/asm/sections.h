179dedb75cf786fab400acb2068877f42dfc694c
Anton Blanchard <anton@samba.org>
2014-03-11 12:15:27 +1100

    powerpc: Fix ABIv2 issue with dereference_function_descriptor
    Don't try and dereference a function descriptor on ABIv2.
    
    Signed-off-by: Anton Blanchard <anton@samba.org>

--- arch/powerpc/include/asm/sections.h
+++ arch/powerpc/include/asm/sections.h
@@ -27,6 +27,7 @@ static inline int overlaps_kernel_text(unsigned long start, unsigned long end)
 		(unsigned long)_stext < end;
 }
 
+#if !defined(_CALL_ELF) || _CALL_ELF != 2
 #undef dereference_function_descriptor
 static inline void *dereference_function_descriptor(void *ptr)
 {
@@ -37,6 +38,7 @@ static inline void *dereference_function_descriptor(void *ptr)
 		ptr = p;
 	return ptr;
 }
+#endif
 
 #endif
 
